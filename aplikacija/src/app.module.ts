dc50325185c4ca31db489b1ac1e655d64ffa5e8b
MilijanaM <64639260+MilijanaM@users.noreply.github.com>
2020-05-03 18:38:43 +0200

    ctdtd

--- aplikacija/src/app.module.ts
+++ aplikacija/src/app.module.ts
@@ -5,6 +5,13 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import {DatabaseConfiguration} from 'config/database.configuration';
 import { Admin } from 'entities/admin.entity';
 import { AdminService } from './services/admin/admin.service';
+import { Category } from 'entities/category.entity';
+import { Gallery } from 'entities/gallery.entity';
+import { Photo } from 'entities/photo.entity';
+import { Product } from 'entities/product.entity';
+import { Inbox } from 'entities/inbox.entity';
+import { News } from 'entities/news.entity';
+import { ProductPrice } from 'entities/product-price.entity';
 
 
 
@@ -19,7 +26,17 @@ import { AdminService } from './services/admin/admin.service';
       username: DatabaseConfiguration.username,
       password: DatabaseConfiguration.password,
       database: DatabaseConfiguration.database,
-      entities: [Admin]
+      entities: [
+        Admin,
+        Category,
+        Gallery,
+        Inbox,
+        Photo,
+        News,
+        ProductPrice,
+        Product
+      
+      ]
     }),
 
     TypeOrmModule.forFeature([Admin])
