7f05604f27e7b5c6e5840bcac5c3b1041a9f7c39
MilijanaM <64639260+MilijanaM@users.noreply.github.com>
2020-06-06 15:53:59 +0200

    078 - React - Axios - proces osvežavanje tokena i 080 - React - Axios - proces osvežavanje tokena - ISPRAVKA

--- front-end3/src/api/api.ts
+++ front-end3/src/api/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
 import { ApiConfig } from '../config/api.config';
 
 export default function api(
@@ -7,39 +7,95 @@ export default function api(
     body: any | undefined,
     ){
 
-        return new Promise((resolve, reject)=> {
+        return new Promise<apiResponse>((resolve)=> {
 
-            axios({
-                method: method,
-                url: path,
-                baseURL: ApiConfig.API_URL,
-                data: JSON.stringify(body),
-                headers: {
-                    'Content-Type': 'applucation.json',
-                    'Authorization': getToken(),
-                } 
-             }).then(res => responseHandler(res, resolve, reject))
-               .catch(err=> reject(err));
+            const requestData=
+                {
+                    method: method,
+                    url: path,
+                    baseURL: ApiConfig.API_URL,
+                    data: JSON.stringify(body),
+                    headers: {
+                        'Content-Type': 'applucation.json',
+                        'Authorization': getToken(),
+                    },
+                 };
+            
+            axios(requestData)
+            .then(res => responseHandler(res, resolve))
+               .catch(async err=>{ 
+                if(err.response.status=== 401){
+                    const newToken = await refreshToken();
+    
+                    if(!newToken){
+                        const response: apiResponse= {
+                            status: 'login',
+                            data: null,
+         
+                        };
+         
+                        return resolve(response);
+                     }
+    
+                     saveToken(newToken);
+    
+                    }
+                    requestData.headers['Authorization']= getToken();
+    
+                    return await repeatRequest(requestData, resolve);
+    
+                   }
+                const response: apiResponse={
+
+                    status: 'error',
+                    data: err
+                    
+                }; 
+                resolve(response)
+
+               });
         });
         
     }
 
-    function responseHandler(
+    interface apiResponse{
+        status: 'ok'| 'error'|'login';
+        data: any;
+    }
+
+    async function responseHandler(
         res: AxiosResponse<any>,
-        resolve: (value?: unknown) => void,
-        reject: (reason?: any) => void
+        resolve: (value?: apiResponse) => void,
+ 
         ){
 
             if(res.status <200 || res.status>=300){
-               // STATUS CODE 401- Bad token
-               //TODO: Refresh tokena i pokusati ponovo
-               // Ne mozemo da osvezimo token, preusmeriti na login
+            
+              
         
-               return reject(res.data);
+               const response: apiResponse= {
+                   status: 'error',
+                   data: res.data,
+
+               };
+
+               return resolve(response);
             }
 
+            let response: apiResponse;
             if(res.data.statusCode <0){
-                return reject(res.data);
+
+              response= {
+                    status: 'login',
+                    data: null,
+                };
+            }else {
+                response ={
+                    status: 'ok',
+                    data: response.data,
+                };
+            }
+                resolve(response);
             }
             resolve(res.data);
 
@@ -54,4 +110,74 @@ export default function api(
 
     function saveToken(token: string){
         localStorage.setItem('api_token', token);
+    }
+
+    function getRefreshToken(): string {
+        const token= localStorage.getItem('api_refresh_token');
+        return token + '';
+
+    }
+
+    function saveRefreshToken(token: string){
+        localStorage.setItem('api_refresh_token', token);
+    }
+
+    async function refreshToken(
+
+        ): Promise <string| null>{
+            const path='auth/admin/refresh';
+            const data={
+                token:getRefreshToken(),
+            }
+
+            
+            const refreshTokenRequestData: AxiosRequestConfig=
+                {
+                    method: 'post',
+                    url: path,
+                    baseURL: ApiConfig.API_URL,
+                    data: JSON.stringify(data),
+                    headers: {
+                        'Content-Type': 'applucation.json',
+                    },
+                 };
+            
+            const refreshTokenResponse: any {data: {token: string| undefined}}= await axios(refreshTokenRequestData);
+            if(!refreshTokenResponse.data.token){
+                return null;
+            }
+            return refreshTokenResponse.data.token;
+
+    }
+
+    async function repeatRequest(requestData: AxiosRequestConfig, resolve: (value?: apiResponse) => void){
+
+        axios(requestData)
+            .then (res =>{
+                let response: apiResponse;
+                if (res.status===401){
+                     response= {
+                        status: 'login',
+                        data: null,
+     
+                    };
+                }else{response= {
+                    status: 'ok',
+                    data: res,
+ 
+                };
+            
+            }
+                return resolve(response);
+
+            })
+            .catch(err =>{
+                const response: apiResponse= {
+                    status: 'error',
+                    data: err,
+ 
+                };
+                return resolve(response);
+
+            });
     }
