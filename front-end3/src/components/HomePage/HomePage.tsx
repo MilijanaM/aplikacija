bab2295b2edfe745702323f500223f461d074bf7
MilijanaM <64639260+MilijanaM@users.noreply.github.com>
2020-06-08 17:12:38 +0200

    azuriranje

--- front-end3/src/components/HomePage/HomePage.tsx
+++ front-end3/src/components/HomePage/HomePage.tsx
@@ -1,126 +1,118 @@
 import React from 'react';
-import {Container, Card, Row, Col} from 'react-bootstrap';
-import 'jquery/dist/jquery.js';
-import 'popper.js/dist/popper.js';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
-import { faHome, faListAlt } from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { MainMenu } from '../MainMenu/MainMenu';
+import { Container, Card, Row, Col } from 'react-bootstrap';
+import { faListAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CategoryType from '../../types/CategoryType';
 import { Redirect, Link } from 'react-router-dom';
-import api, {ApiResponse} from '../../api/api';
-
+import api, { ApiResponse } from '../../api/api';
 
 interface HomePageState {
-    isAdminLoggedIn: boolean;
-    categories: CategoryType[];
+    isUserLoggedIn: boolean;
+    categories?: CategoryType[];
 }
-interface ApiCategoryDto{
-  categoryId: number;
-  name: string;
+
+interface ApiCategoryDto {
+    categoryId: number;
+    name: string;
 }
 
 class HomePage extends React.Component {
-   state: HomePageState;
-  setAdminLoggedInState: any;
+    state: HomePageState;
 
-   constructor(props: Readonly<{}>){
-     super(props);
+    constructor(props: Readonly<{}>) {
+        super(props);
 
-     this.state={
-       isAdminLoggedIn: true,
-       categories: [],
+        this.state = {
+            isUserLoggedIn: true,
+            categories: [],
+        };
+    }
 
-     };
-   }
+    componentWillMount() {
+        this.getCategories();
+    }
 
-   componentWillMount(){
-     this.getCategories();
-   }
+    componentWillUpdate() {
+        this.getCategories();
+    }
 
-   componentWillUpdate(){
-     this.getCategories();
-   }
+    private getCategories() {
+        api('api/category/?filter=parentCategoryId||$isnull ', 'get', {})
+        .then((res: ApiResponse) => {
+            if (res.status === "error" || res.status === "login") {
+                this.setLogginState(false);
+                return;
+            }
 
-   private getCategories(){
-       api('api/category/', 'get', {})
-       .then((res: ApiResponse)=>{
-          if(res.status==="error"|| res.status==="login"){
-            this.setAdminLoggedInState(false);
-            return;
-          }
-          this.putCategoriesInState(res.data);
-      });
+            this.putCategoriesInState(res.data);
+        });
+    }
 
-   }
-   private putCategoriesInState(data: ApiCategoryDto[]){
-       const categories: CategoryType[]= data.map(category =>{
-          return{
-            categoryId: category.categoryId,
-            name: category.name,
-            items: [],
+    private putCategoriesInState(data?: ApiCategoryDto[]) {
+        const categories: CategoryType[] | undefined = data?.map(category => {
+            return {
+                categoryId: category.categoryId,
+                name: category.name,
+                items: [],
+            };
+        });
 
-          };
-       });
-       const newState = Object.assign(this.state, {
-        categories: categories,
-    });
+        const newState = Object.assign(this.state, {
+            categories: categories,
+        });
 
         this.setState(newState);
-   }
-
-   private setAdminLogginState(isLoggedIn: boolean) {
-    const newState = (Object.assign(this.state, {
-      isAdminLoggedIn: isLoggedIn,
-    }));
-
-    this.setState(newState);
-  }
-
-  render(){
-    if (this.state.isAdminLoggedIn=== false) {
-      return (
-          <Redirect to="/admin/login" />
-      );
     }
-      return (
-        <Container>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <FontAwesomeIcon icon={ faHome } /> Home Page
-              </Card.Title>
-  
-              <Row>
-                { this.state.categories.map(this.renderSingleCategory) }
-              </Row>
-            </Card.Body>
-          </Card>
-        </Container>
-      );
-    
-  }
 
-  private renderSingleCategory(category: CategoryType) {
-    return (
-      <Col xs="12" sm="6" md="4" lg="3">
-        <Card className="mb-3">
-          <Card.Body>
-            <Card.Title as="p">
-              <strong>
-                { category.name }
-              </strong>
-            </Card.Title>
-            <Link to={ `category/${ category.categoryId }/` }
-                  className="btn btn-sm btn-primary btn-block">
-              Click to open
-            </Link>
-          </Card.Body>
-        </Card>
-      </Col>
-    );
-  }
+    private setLogginState(isLoggedIn: boolean) {
+        const newState = Object.assign(this.state, {
+            isUserLoggedIn: isLoggedIn,
+        });
+
+        this.setState(newState);
+    }
+
+    render() {
+        if (this.state.isUserLoggedIn === false) {
+            return (
+                <Redirect to="/user/login" />
+            );
+        }
+
+        return (
+            <Container>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>
+                            <FontAwesomeIcon icon={ faListAlt } /> Top level categories
+                        </Card.Title>
+
+                        <Row>
+                            { this.state.categories?.map(this.singleCategory) }
+                        </Row>
+                    </Card.Body>
+                </Card>
+            </Container>
+        );
+    }
+
+    private singleCategory(category: CategoryType) {
+        return (
+            <Col lg="3" md="4" sm="6" xs="12">
+                <Card className="mb-3">
+                    <Card.Body>
+                        <Card.Title as="p">
+                            { category.name }
+                        </Card.Title>
+                        <Link to={ `/category/${ category.categoryId }` }
+                              className="btn btn-primary btn-block btn-sm">
+                            Open category
+                        </Link>
+                    </Card.Body>
+                </Card>
+            </Col>
+        );
+    }
 }
 
 export default HomePage;
