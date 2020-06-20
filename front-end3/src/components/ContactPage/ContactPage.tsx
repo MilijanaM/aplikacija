d411a1752da94d4453456f0b2575a2223c5847c9
MilijanaM <64639260+MilijanaM@users.noreply.github.com>
2020-06-20 10:45:59 +0200

    contact page

--- front-end3/src/components/ContactPage/ContactPage.tsx
+++ front-end3/src/components/ContactPage/ContactPage.tsx
@@ -1,26 +1,69 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { Container, Card } from 'react-bootstrap';
+import { Container, Card, Form as BootstrapForm, Button, Row, Col } from 'react-bootstrap';
 
-export default class ContactPage extends React.Component{
+import { Formik, Form, Field, ErrorMessage } from "formik"
 
-    render(){
-        return(
-        <Container>
-            <Card>
-               <Card.Body>
-                   <Card.Title>
-                   <FontAwesomeIcon icon={faPhone}/> Contact details 
-                   </Card.Title>
-                   <Card.Text>
-                       Contact details will be show here...
-                   </Card.Text>
-               </Card.Body>
-            </Card>
-           
-            
-        </Container>
+export default class ContactPage extends React.Component {
+
+    render() {
+        return (
+            <Container>
+                <Formik
+                    initialValues={{
+                        title: '',
+                        fullName: '',
+                        email: '',
+                        text: '',
+                    }}
+                    onSubmit={(values, { setSubmitting }) => {
+                        setTimeout(() => {
+                            alert(JSON.stringify(values, null, 2));
+                            setSubmitting(false);
+                        }, 400);
+                    }}
+                >
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <Container>
+                                <Row className="justify-content-center"  >
+                                    <Col lg="5" md="6" sm="12" xs="12" >
+                                        <Card>
+                                            <Card.Body>
+                                                <Card.Title>
+                                                    <FontAwesomeIcon icon={faPhone} /> Contact details
+                                    </Card.Title>
+
+                                                <BootstrapForm.Group>
+                                                    <BootstrapForm.Label>Title</BootstrapForm.Label>
+                                                    <Field type="text" className="form-control" name="title" />
+                                                </BootstrapForm.Group>
+                                                <BootstrapForm.Group>
+                                                    <BootstrapForm.Label>Full Name</BootstrapForm.Label>
+                                                    <Field type="text" className="form-control" name="fullName" />
+                                                </BootstrapForm.Group>
+                                                <BootstrapForm.Group>
+                                                    <BootstrapForm.Label>Email</BootstrapForm.Label>
+                                                    <Field type="text" className="form-control" name="email" />
+                                                </BootstrapForm.Group>
+                                                <BootstrapForm.Group>
+                                                    <BootstrapForm.Label>Text</BootstrapForm.Label>
+                                                    <Field type="text" as="textarea" className="form-control" rows="3" name="text" />
+                                                </BootstrapForm.Group>
+                                                <Button variant="primary" disabled={isSubmitting} type="submit">Submit</Button>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+
+                                </Row>
+                            </Container>
+                        </Form>
+                    )}
+                </Formik>
+
+
+            </Container>
         );
     }
 
